75e295c069b126a8d569e1a41847a33006780c73
Elias Lorenz Schaut <uvmvb@student.kit.edu>
2021-05-07 17:15:46 +0200

    fill init command

--- commands/build/init.js
+++ commands/build/init.js
@@ -1,3 +1,5 @@
+const { category_name, lobby_name } = require("../../config/config.json").init
+
 module.exports = {
     name: 'init',
     description: 'Create a new lobby',
@@ -8,6 +10,24 @@ module.exports = {
     dmOnly: false,
     restricted: true,
     execute(message, args) {
-
+        message.guild.channels.create(category_name, {
+            type: 'category',
+            position: 1,
+            permissionOverwrites: [
+                {
+                    id: message.guild.id,
+                    allow: ['VIEW_CHANNEL'],
+                }]
+        }).then(cat => {
+            message.guild.channels.create(lobby_name, {
+                type: 'voice',
+                parent: cat,
+                permissionOverwrites: [
+                    {
+                        id: message.guild.id,
+                        allow: ['VIEW_CHANNEL'],
+                    }]
+            })
+        })
     },
 };
